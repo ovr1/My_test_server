e3fa0d53ef72145a4386380ecf86d861342c3568
ovr1 <rybynok@mail.ru>
2019-04-12 01:17:22 +0300

    test

---
+++ server.py
@@ -0,0 +1,24 @@
+from bottle import route, run, view
+from datetime import datetime as dt
+from random import random
+
+@route("/")
+@view("predictions")
+def index():
+  now = dt.now()
+
+  x = random()
+
+  return {
+    "date": f"{now.year}-{now.month}-{now.day}",
+    "predictions": [
+      "После обеда ожидайте неожиданного праздника.",
+      "Днем остерегайтесь неожиданного праздника.",
+      "Утром ожидайте гостей из забытого прошлого.",
+    ]
+  }
+run(
+  host="localhost",
+  port=8080,
+  autoreload=True
+)
---
+++ views/predictions.tpl
@@ -0,0 +1,15 @@
+<html><head>
+  <meta charset='utf-8'>
+  <title>Гороскоп на сегодня</title>
+
+  <body>
+    <div class="container">
+      <h1>Что день {{ date }} готовит</h1>
+
+      % for pred in predictions:
+      <p>{{ pred }}</p>
+      % end
+
+    </div>
+  </body>
+</html>
