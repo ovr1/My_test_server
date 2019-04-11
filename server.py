f9813efe1a27219a7c96eef0dcfc634d2ca445c6
ovr1 <rybynok@mail.ru>
2019-04-12 02:06:31 +0300

    some text

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from bottle import route, run, view
 from datetime import datetime as dt
 from random import random
@@ -17,8 +18,7 @@ def index():
       "Утром ожидайте гостей из забытого прошлого.",
     ]
   }
-run(
-  host="localhost",
-  port=8080,
-  autoreload=True
-)
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
